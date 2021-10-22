bfb89553161adc02617dbf12767aa4e223bdb941
sergio rueda <srueda@smartdataautomation.com>
2021-10-21 19:47:27 -0500

    app categories finished

--- blog/urls.py
+++ blog/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from categories.api.router import router_categories
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -36,4 +37,5 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('users.api.router')),
+    path('api/', include(router_categories.urls)),
 ]
--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=200, unique=True)
+    published = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return '{}'.format(self.title)
+    
